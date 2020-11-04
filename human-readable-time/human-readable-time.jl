67d442de20976cb7979a724310d87d63f5649926
Marielle <marielle@widged.com>
2020-11-04 14:00:11 +0100

    added a question

--- human-readable-time/human-readable-time.jl
+++ human-readable-time/human-readable-time.jl
@@ -1,11 +1,15 @@
-"convert a number of seconds into a HH:MM:SS string"
 
+"pad an integer to get to a 00 format"
+"""
+:Q: what are, in julia, the pros and cons of declaring this utility
+function outside the humanreadable function or nesting it inside it?
+"""
 function nn(n)
     return lpad(string(n), 2, '0')
 end
 
+"convert a number of seconds into a HH:MM:SS string"
 function humanreadable(seconds)
-    # Hours, minutes and seconds
     hrs = seconds ÷ 3600;
     mins = (seconds % 3600) ÷ 60;
     secs = seconds % 60;
